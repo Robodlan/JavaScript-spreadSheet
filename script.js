24f5174387e35cb0ce2b526c2207960262c3c5ea
Robodlan <reinaldo.robles@icloud.com>
2024-03-08 12:18:30 -0400

    const letter

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const range = (start, end)=> Array(end - start + 1).fill(start).map((element, index)=> element + index);
-const charRange = (start, end) => range(start, end);
+const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
 window.onload = () => {
     const container = document.getElementById("container");
@@ -9,5 +9,15 @@ window.onload = () => {
         label.textContent = name;
         container.appendChild(label);
     }
+    const letters = charRange("A", "J");
+    letters.forEach(createLabel);
+    range(1, 99).forEach(number => {
+        createLabel(number);
+        letters.forEach(letter => {
+            const input = document.createElement("input");
+            input.type = "text";
+            input.id = letter + number;
+        })
+    });
 };
 
