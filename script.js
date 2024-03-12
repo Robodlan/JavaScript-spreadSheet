2d26b313dcdd2601d52f417957c2be4cbcdcec8a
Robodlan <reinaldo.robles@icloud.com>
2024-03-12 07:21:40 -0400

    update elemvalue

--- script.js
+++ script.js
@@ -20,13 +20,10 @@ const evalFormula = (x, cells) => {
     const idToText = id => cells.find(cell => cell.id === id).value;
     const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
     const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
-    const elemValue = num => {
-        const inner = character => {
-         return idToText(character + num);
-        };
-        return inner 
-    };
-    const addCharacters = (character1) => character2 => num => {};
+    
+    const elemValue = num =>  character => idToText(character + num);
+
+    const addCharacters = (character1) => character2 => num => charRange(character1, character2);
   }
 
 window.onload = () => {
