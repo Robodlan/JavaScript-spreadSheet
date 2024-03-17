4c7a6bbd26d4bed82df83b1d33fa667c69633f17
Robodlan <reinaldo.robles@icloud.com>
2024-03-17 18:10:59 -0400

    applyFn

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const infixEval = (str, regex) => str.replace(regex, (_match, arg1,operator,arg2
 const highPrecedence = (str) => {
     const regex = /([\d.]+)([*\/])([\d.]+)/;
     const str2 = infixEval(str,regex);
-    return str2 === str ? str :  highPrecedence(str2);
+  return str2 === str ? str :  highPrecedence(str2);
 };
 
 const isEven = num => num % 2 === 0;
@@ -27,6 +27,15 @@ const median = (nums) => {
   };
 
   const spreadsheetFunctions = { sum, average, median };
+
+  const applyFunction = (str) => {
+    const noHigh = highPrecedence(str);
+    const infix = /([\d.]+)([+-])([\d.]+)/;
+    const str2 = infixEval(noHigh, infix);
+    const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+    const toNumberList = (args) => args.split(",").map(parseFloat);
+    const apply = (fn ,args) => {};
+  };
   
 const range = (start, end)=> Array(end - start + 1).fill(start).map((element, index)=> element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
